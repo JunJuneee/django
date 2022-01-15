4e103a4aa4e9f79dd7d4faf14aada646bad9b69a
JunJuneee <hanjunt5722@gmail.com>
2022-01-15 13:31:37 +0900

    database and migrations

--- tutorial/django_tutorial/blog/views.py
+++ tutorial/django_tutorial/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'nana',
-        'title': 'blog 1',
-        'content': 'first content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'nunu',
-        'title': 'blog 2',
-        'content': '2 content',
-        'date_posted': 'August 27, 2018'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
