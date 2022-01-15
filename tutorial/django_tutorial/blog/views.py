f24c87d588f1b328295a99fe0458cb3a3ab3a22d
JunJuneee <hanjunt5722@gmail.com>
2022-01-15 13:01:07 +0900

    template

--- tutorial/django_tutorial/blog/views.py
+++ tutorial/django_tutorial/blog/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+    {
+        'author': 'nana',
+        'title': 'blog 1',
+        'content': 'first content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'nunu',
+        'title': 'blog 2',
+        'content': '2 content',
+        'date_posted': 'August 27, 2018'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
