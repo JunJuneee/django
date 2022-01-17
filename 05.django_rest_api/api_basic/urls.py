9f7beaf37b968dee0fd282294846bbfb91ae5380
JunJuneee <hanjunt5722@gmail.com>
2022-01-17 15:30:01 +0900

    REST Generic Views & Mixins

--- 05.django_rest_api/api_basic/urls.py
+++ 05.django_rest_api/api_basic/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import article_list, article_detail
+from .views import ArticleAPIView,  ArticleDetails, GenericAPIView
 urlpatterns = [
-    path('article/', article_list),
-    path('article/<int:pk>/', article_detail),
+    path('article/', ArticleAPIView.as_view()),
+    path('generic/article/<int:id>/', GenericAPIView.as_view()),
+    path('article/<int:id>/', ArticleDetails.as_view()),
 ]
